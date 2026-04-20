173956eae64325e470e9ab509b5094f564291beb
Eystein Waade <eystein.waade@nav.no>
2026-04-20 14:00:08 +0200

    ♻️ Legg view-tilgang til datasettet pen_dataprodukt_dataset som separat ressurs for å unngå 🐓🥚 -problemete
    Co-authored-by: Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>

--- prod/pen-dataprodukt-dataset.tf
+++ prod/pen-dataprodukt-dataset.tf
@@ -17,22 +17,6 @@ resource "google_bigquery_dataset" "pen_dataprodukt_dataset" {
     special_group = "projectWriters"
   }
   access {
-    view {
-      dataset_id = google_bigquery_dataset.saksbehandlingsstatistikk_til_team_sak_dataset.dataset_id
-      project_id = var.gcp_project["project"]
-      table_id   = "saksbehandlingsstatistikk_ufore_view"
-    }
-  }
-
-  access {
-    view {
-      dataset_id = google_bigquery_dataset.saksbehandlingsstatistikk_til_team_sak_dataset.dataset_id
-      project_id = var.gcp_project["project"]
-      table_id   = "saksbehandlingsstatistikk_alder_view"
-    }
-  }
-
-  access {
     role          = "WRITER"
     user_by_email = google_service_account.bigquery_airflow_dvh.email
   }
@@ -71,4 +55,28 @@ resource "google_bigquery_table" "saksbehandlingsstatistikk_alder" {
 
   schema = file("${path.module}/../schemas/saksbehandlingsstatistikk.json")
 
+}
+
+resource "google_bigquery_dataset_access" "pen_dataprodukt_ufore_view_access" {
+  dataset_id    = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  project       = var.gcp_project["project"]
+  view {
+    dataset_id = google_bigquery_dataset.saksbehandlingsstatistikk_til_team_sak_dataset.dataset_id
+    project_id = var.gcp_project["project"]
+    table_id   = module.saksbehandlingsstatistikk_ufore_til_team_sak_view.bigquery_view_id
+  }
+
+  depends_on = [module.saksbehandlingsstatistikk_ufore_til_team_sak_view]
+}
+
+resource "google_bigquery_dataset_access" "pen_dataprodukt_alder_view_access" {
+  dataset_id    = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  project       = var.gcp_project["project"]
+  view {
+    dataset_id = google_bigquery_dataset.saksbehandlingsstatistikk_til_team_sak_dataset.dataset_id
+    project_id = var.gcp_project["project"]
+    table_id   = module.saksbehandlingsstatistikk_alder_til_team_sak_view.bigquery_view_id
+  }
+
+  depends_on = [module.saksbehandlingsstatistikk_alder_til_team_sak_view]
 }
