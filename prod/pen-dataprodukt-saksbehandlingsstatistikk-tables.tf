fb1cd0d8de004fb515bfae38ca8566c8e5813208
Eystein Waade <eystein.waade@nav.no>
2026-04-22 09:28:13 +0200

    ♻️ separer tabelldefinisjoner og dataset definisjon for pen-dataprodukt-dataset
    Co-authored-by: Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>

---
+++ prod/pen-dataprodukt-saksbehandlingsstatistikk-tables.tf
@@ -0,0 +1,55 @@
+resource "google_bigquery_table" "saksbehandlingsstatistikk_ufore" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  table_id   = "saksbehandlingsstatistikk_ufore"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = file("${path.module}/../schemas/saksbehandlingsstatistikk.json")
+
+}
+
+resource "google_bigquery_table" "saksbehandlingsstatistikk_alder" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  table_id   = "saksbehandlingsstatistikk_alder"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = file("${path.module}/../schemas/saksbehandlingsstatistikk.json")
+
+}
+
+resource "google_bigquery_dataset_access" "pen_dataprodukt_ufore_view_access" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  project    = var.gcp_project["project"]
+  view {
+    dataset_id = google_bigquery_dataset.saksbehandlingsstatistikk_til_team_sak_dataset.dataset_id
+    project_id = var.gcp_project["project"]
+    table_id   = module.saksbehandlingsstatistikk_ufore_til_team_sak_view.bigquery_view_id
+  }
+
+  depends_on = [module.saksbehandlingsstatistikk_ufore_til_team_sak_view]
+}
+
+resource "google_bigquery_dataset_access" "pen_dataprodukt_alder_view_access" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  project    = var.gcp_project["project"]
+  view {
+    dataset_id = google_bigquery_dataset.saksbehandlingsstatistikk_til_team_sak_dataset.dataset_id
+    project_id = var.gcp_project["project"]
+    table_id   = module.saksbehandlingsstatistikk_alder_til_team_sak_view.bigquery_view_id
+  }
+
+  depends_on = [module.saksbehandlingsstatistikk_alder_til_team_sak_view]
+}
---
+++ prod/pen-dataprodukt-stonadsstatistikk-tables.tf
@@ -0,0 +1,83 @@
+resource "google_bigquery_table" "stonadsstatistikk_alder_belop" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  table_id   = "stonadsstatistikk_alder_belop"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = file("${path.module}/../schemas/stonadsstatistikk_alder_belop.json")
+
+}
+
+resource "google_bigquery_table" "stonadsstatistikk_alder_beregning" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  table_id   = "stonadsstatistikk_alder_beregning"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = file("${path.module}/../schemas/stonadsstatistikk_alder_beregning.json")
+
+}
+
+resource "google_bigquery_table" "stonadsstatistikk_alder_vedtak" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  table_id   = "stonadsstatistikk_alder_vedtak"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = file("${path.module}/../schemas/stonadsstatistikk_alder_vedtak.json")
+
+}
+
+resource "google_bigquery_dataset_access" "pen_dataprodukt_stonadsstatistikk_alder_belop_view_access" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  project    = var.gcp_project["project"]
+  view {
+    dataset_id = google_bigquery_dataset.stonadsstatistikk_til_team_pensjon_dvh_dataset.dataset_id
+    project_id = var.gcp_project["project"]
+    table_id   = module.stonadsstatistikk_alder_belop_til_team_pensjon_dvh_view.bigquery_view_id
+  }
+
+  depends_on = [module.stonadsstatistikk_alder_belop_til_team_pensjon_dvh_view]
+}
+
+resource "google_bigquery_dataset_access" "pen_dataprodukt_stonadsstatistikk_alder_beregning_view_access" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  project    = var.gcp_project["project"]
+  view {
+    dataset_id = google_bigquery_dataset.stonadsstatistikk_til_team_pensjon_dvh_dataset.dataset_id
+    project_id = var.gcp_project["project"]
+    table_id   = module.stonadsstatistikk_alder_beregning_til_team_pensjon_dvh_view.bigquery_view_id
+  }
+
+  depends_on = [module.stonadsstatistikk_alder_beregning_til_team_pensjon_dvh_view]
+}
+
+resource "google_bigquery_dataset_access" "pen_dataprodukt_stonadsstatistikk_alder_vedtak_view_access" {
+  dataset_id = google_bigquery_dataset.pen_dataprodukt_dataset.dataset_id
+  project    = var.gcp_project["project"]
+  view {
+    dataset_id = google_bigquery_dataset.stonadsstatistikk_til_team_pensjon_dvh_dataset.dataset_id
+    project_id = var.gcp_project["project"]
+    table_id   = module.stonadsstatistikk_alder_vedtak_til_team_pensjon_dvh_view.bigquery_view_id
+  }
+
+  depends_on = [module.stonadsstatistikk_alder_vedtak_til_team_pensjon_dvh_view]
+}
